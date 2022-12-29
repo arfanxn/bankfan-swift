e6b2fe1a5b0ce8f5f207f753fd2bf5aba34883db
arfanxn <arfan2173@gmail.com>
2022-12-29 21:21:13 +0700

    Initial Commit

---
+++ Bankfan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankfan
+//
+//  Created by Muhammad Arfan on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
